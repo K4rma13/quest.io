6e79fc2431027730e28cf8a140520320d63db99f
K4rma13 <pedrodviana@hotmail.com>
2025-12-05 01:57:05 +0000

    update: Changed db structure to add questions and save their answers

--- createTables.sql
+++ createTables.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS quest;
 USE quest;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user(
     id INT AUTO_INCREMENT NOT NULL,
     username VARCHAR(25) NOT NULL,
     email VARCHAR(50) NOT NULL,
@@ -11,8 +11,16 @@ CREATE TABLE user (
     UNIQUE(email)
 );
 
+CREATE TABLE IF NOT EXISTS questionaire(
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(25) NOT NULL,
+    ownerid INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(ownerid)
+        REFERENCES user(id)
+);
 
-CREATE TABLE rooms (
+CREATE TABLE IF NOT EXISTS rooms (
     id INT AUTO_INCREMENT NOT NULL,
     label VARCHAR(25) NOT NULL,
     descr VARCHAR(200) NOT NULL,
@@ -20,7 +28,29 @@ CREATE TABLE rooms (
     ownerid INT NOT NULL,
     available BOOLEAN NOT NULL,
     capacity INT NOT NULL,
+    quest_id INT NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY(ownerid)
-        REFERENCES user(id)
+        REFERENCES user(id),
+    FOREIGN KEY(quest_id)
+        REFERENCES questionaire(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS question(
+    questionaire_id INT NOT NULL,
+    ord INT NOT NULL,
+    quest VARCHAR(100) NOT NULL,
+    answers VARCHAR(500) NOT NULL,
+    FOREIGN KEY(questionaire_id)
+        REFERENCES questionaire(id)
+);
+DROP TABLE answer;
+CREATE TABLE IF NOT EXISTS answer(
+    room_id INT NOT NULL,
+    answeredby VARCHAR(25) NOT NULL,
+    choice INT NOT NULL,
+    ord INT NOT NULL,
+    FOREIGN KEY(room_id)
+        REFERENCES rooms(id)
 );
--- testUser.sql
+++ testUser.sql
@@ -1,6 +1,15 @@
 INSERT INTO user (username,email,passwd) VALUES ('Testeuser','kaka@gmail.com','$2b$10$5Z9IBbOOqM44NWwYrq9zqOMvu1FFe5.kwwebiCVVRixItDmMCWHgC');
 
+INSERT INTO questionaire (title ,ownerid) VALUES ("Questionario Teste",1);
 
-INSERT INTO rooms (label,descr,ownerid,available,capacity) VALUES ('nome top','sala para testes',1,1,4);
-INSERT INTO rooms (label,descr,ownerid,available,capacity) VALUES ('Nome ainda mais top','sala testes',1,1,4);
-INSERT INTO rooms (label,descr,passwd,ownerid,available,capacity) VALUES ('Average sala','testes imprevisiveis','batatinha',1,1,4);
+--@block
+INSERT INTO question (questionaire_id, ord, quest, answers) VALUES (1,1,"Maior burro","Santos;Juan;Jota");
+INSERT INTO question (questionaire_id, ord, quest, answers) VALUES (1,2,"Maior burra","Filipa;Maggie;Maria;Ines");
+
+
+INSERT INTO rooms (label,descr,ownerid,available,capacity,quest_id) VALUES ('nome top','sala para testes',1,1,4,1);
+INSERT INTO rooms (label,descr,ownerid,available,capacity,quest_id) VALUES ('Nome ainda mais top','sala testes',1,1,4,1);
+INSERT INTO rooms (label,descr,passwd,ownerid,available,capacity,quest_id) VALUES ('Average sala','testes imprevisiveis','batatinha',1,1,4,1);
+
+--@block
+INSERT INTO answer (questionaire_id,answeredby,choice,ord) VALUES (1,"Jose",1,1);
